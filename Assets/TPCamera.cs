dd8e562ffc52b04d809888f289308cc1067c2dbb
David Liljefors <david.liljefors@futuregames.nu>
2020-03-06 18:10:23 +0100

    camera return to back if you drag it and leave for 2 seconds

--- Assets/TPCamera.cs
+++ Assets/TPCamera.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Collections;
 
 public class TPCamera : MonoBehaviour
 {
@@ -13,38 +15,52 @@ public class TPCamera : MonoBehaviour
 	private float maxRange = 10.0f;
 	private float currentRange;
 
+	[Range(5f, 100f)]
+	[SerializeField]
+	private float lerpSpeed = 10f;
+
 	private CursorLockMode oldLockMode;
 	private Vector2 oldMousePos;
 	private Vector3 offset;
 	private Quaternion cameraRot;
 	private Quaternion focusRot;
+	private bool useTargetRotation = true;
+	private Coroutine resetRotationRoutine;
+
+
 
 	void Start()
 	{
 		focus = new GameObject("CameraFocus").transform;
 		focus.position = objectToFollow.transform.position;
-		focus.parent = objectToFollow.transform;
-
 		offset = new Vector3();
+
 		currentRange = maxRange;
 		cameraRot = transform.localRotation;
 		focusRot = focus.localRotation;
 	}
-
 	void LateUpdate()
 	{
+		focus.position = objectToFollow.transform.position;
+
 		if (Input.mouseScrollDelta.y != 0.0f)
 		{
 			currentRange -= Input.mouseScrollDelta.y;
-			currentRange = Mathf.Clamp(currentRange, 0, maxRange);
+			currentRange = Mathf.Clamp(currentRange, 0.1F, maxRange);
 		}
 
 
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
+			useTargetRotation = false;
 			oldMousePos = Input.mousePosition;
 			oldLockMode = Cursor.lockState;
 			Cursor.lockState = CursorLockMode.Locked;
+			if (resetRotationRoutine != null)
+			{
+				StopCoroutine(resetRotationRoutine);
+				resetRotationRoutine = null;
+			}
 		}
 
 		if (Input.GetKey(KeyCode.Mouse0))
@@ -55,11 +71,12 @@ public class TPCamera : MonoBehaviour
 			cameraRot *= Quaternion.Euler(-xRot, 0F, 0F);
 			cameraRot = ClampRotationAroundXAxis(cameraRot);
 			focusRot *= Quaternion.Euler(0F, yRot, 0F);
-
+			focus.rotation = focusRot;
 		}
 
 		if (Input.GetKeyUp(KeyCode.Mouse0))
 		{
+			resetRotationRoutine = StartCoroutine(EnableCameraFollowRotation());
 			Cursor.lockState = oldLockMode;
 		}
 
@@ -78,6 +95,8 @@ public class TPCamera : MonoBehaviour
 			cameraRot *= Quaternion.Euler(-xRot, 0F, 0F);
 			cameraRot = ClampRotationAroundXAxis(cameraRot);
 			focusRot *= Quaternion.Euler(0F, yRot, 0F);
+			focus.rotation = focusRot;
+			objectToFollow.transform.rotation = Quaternion.Euler(0, focus.rotation.eulerAngles.y, 0);
 		}
 
 		if (Input.GetKeyUp(KeyCode.Mouse1))
@@ -85,26 +104,32 @@ public class TPCamera : MonoBehaviour
 			Cursor.lockState = oldLockMode;
 		}
 
-		transform.localRotation = cameraRot;
-		focus.localRotation = focusRot;
+		transform.rotation = cameraRot;
 
+		if (useTargetRotation)
+		{
+			focus.rotation = objectToFollow.transform.rotation;
+		}
 		// Calculate offset from focus to camera. Set position and rotation
 		offset = new Vector3(0, 0, -currentRange);
 		offset = transform.rotation * offset;
 		offset = focus.rotation * offset;
-		transform.position = offset + focus.position;
-		transform.rotation = Quaternion.LookRotation(focus.position - transform.position);
 
 
-		if (Input.GetKey(KeyCode.Mouse1))
+		if (Physics.Linecast(focus.position, offset + focus.position, out RaycastHit hit))
 		{
-			objectToFollow.transform.rotation = Quaternion.Euler(0,focus.rotation.eulerAngles.y, 0);
-			focus.localRotation = Quaternion.identity;
-			focusRot = Quaternion.identity;
+			transform.position = hit.point;
 		}
+		else
+		{
+			transform.position = Vector3.Lerp(transform.position, offset + focus.position, lerpSpeed * Time.deltaTime);
+		}
+
+		transform.rotation = Quaternion.LookRotation(focus.position - transform.position);
+		focusRot = focus.rotation;
 	}
 
-	public float MinimumX = -80;
+	public float MinimumX = -0;
 	public float MaximumX = 80;
 	Quaternion ClampRotationAroundXAxis(Quaternion q)
 	{
@@ -118,5 +143,17 @@ public class TPCamera : MonoBehaviour
 		q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
 
 		return q;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawLine(focus.position, focus.position + offset);
+		Gizmos.DrawWireSphere(focus.position, 0.5F);
+	}
+	IEnumerator EnableCameraFollowRotation()
+	{
+		yield return new WaitForSeconds(2F);
+		useTargetRotation = true;
+		resetRotationRoutine = null;
 	}
 }
