c1998bf8ab7c85dfc663aea090c2bfede9f7e67b
David Liljefors <david.liljefors@futuregames.nu>
2020-03-13 14:11:27 +0100

    Fix bugs, made ready to turn in assignment

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 6.0F;
+
+    [SerializeField] private float jumpSpeed = 8.0F;
+
+    [SerializeField] private float gravity = 20.0F;
+
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = Input.GetMouseButton(1)
+                ? new Vector3(Input.GetAxisRaw("Turn"), 0, Input.GetAxisRaw("Forward"))
+                : new Vector3(Input.GetAxisRaw("Strafe"), 0, Input.GetAxisRaw("Forward"));
+
+            if (moveDirection.sqrMagnitude > 1f)
+            {
+                moveDirection.Normalize();
+            }
+            moveDirection = transform.rotation * moveDirection;
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        if (!Input.GetMouseButton(1))
+        {
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxisRaw("Turn"), 0);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TPCamera.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TPCamera : MonoBehaviour
+{
+    #region Serialized Variables
+
+    [Tooltip("The GameObject this camera should follow")] [SerializeField]
+    private GameObject objectToFollow = default;
+
+    [Range(5f, 100f)] [SerializeField] private float maxRange = 10.0f;
+
+    [Range(0.01f, 10f)] [SerializeField] private float minRange = 0.1f;
+
+    [Range(1f, 50f)] [SerializeField] private float lerpSpeed = 10f;
+
+    [Range(0f, -90f)] [SerializeField] private float minX = -90f;
+
+    [Range(0f, 90f)] [SerializeField] private float maxX = 90f;
+
+    [Tooltip("The time until camera resets to behind after user let go of LMB")] [SerializeField]
+    private float resetTime = 2f;
+
+    [Tooltip("Size of the sphere for collision check with obstacles")] [SerializeField]
+    private float sphereSize = 0.25f;
+
+    [SerializeField] private string axisYName = "Mouse Y";
+    [SerializeField] private string axisXName = "Mouse X";
+
+    #endregion Serialized Variables
+
+    #region Private Variables
+
+    private float currentRange;
+    private Transform focus = default;
+    private Quaternion cameraRot;
+    private Quaternion focusRot;
+    private Vector3 offset;
+    private CursorLockMode oldLockMode;
+    private bool useTargetRotation = true;
+
+    private Coroutine resetRotationRoutine;
+
+    #endregion Private Variables
+
+    private void Start()
+    {
+        focus = new GameObject("CameraFocus").transform;
+        focus.position = objectToFollow.transform.position;
+        offset = new Vector3();
+
+        currentRange = maxRange;
+        cameraRot = transform.rotation;
+        focusRot = focus.rotation;
+    }
+
+    private void LateUpdate()
+    {
+        focus.position = objectToFollow.transform.position;
+
+        HandleInput();
+        UpdateCamera();
+    }
+
+    private void UpdateCamera()
+    {
+        transform.rotation = cameraRot;
+
+        if (useTargetRotation)
+        {
+            focus.rotation = Quaternion.Lerp(focus.rotation, objectToFollow.transform.rotation,
+                Time.deltaTime * lerpSpeed);
+        }
+
+        offset.x = 0;
+        offset.y = 0;
+        offset.z = -currentRange;
+
+        offset = transform.rotation * offset;
+        offset = focus.rotation * offset;
+
+        // Sweep a sphere towards camera position. If a hit occurs, 
+        // calculate length of that vector in direction of camera and change offset length to that instead
+
+        if (Physics.SphereCast(focus.position, sphereSize, transform.position - focus.position, out RaycastHit hit,
+            currentRange))
+        {
+            float vectorLength =
+                Mathf.Sqrt(Vector3.Dot((hit.point - focus.position), (transform.position - focus.position)));
+            offset = offset.normalized * vectorLength;
+        }
+
+        transform.position = offset + focus.position;
+        transform.rotation = Quaternion.LookRotation(focus.position - transform.position);
+        focusRot = focus.rotation;
+    }
+
+    private void HandleInput()
+    {
+        currentRange -= Input.mouseScrollDelta.y;
+        currentRange = Mathf.Clamp(currentRange, minRange, maxRange);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !Input.GetKey((KeyCode.Mouse1)))
+        {
+            useTargetRotation = false;
+            oldLockMode = Cursor.lockState;
+            Cursor.lockState = CursorLockMode.Locked;
+            if (resetRotationRoutine != null)
+            {
+                StopCoroutine(resetRotationRoutine);
+                resetRotationRoutine = null;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && !Input.GetKey(KeyCode.Mouse1))
+        {
+            float xRot = Input.GetAxis(axisYName);
+            float yRot = Input.GetAxis(axisXName);
+
+            cameraRot *= Quaternion.Euler(-xRot, 0F, 0F);
+            cameraRot = ClampRotationAroundXAxis(cameraRot);
+            focusRot *= Quaternion.Euler(0F, yRot, 0F);
+            focus.rotation = focusRot;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && !Input.GetKey((KeyCode.Mouse1)))
+        {
+            resetRotationRoutine = StartCoroutine(EnableCameraFollowRotation());
+            Cursor.lockState = oldLockMode;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && !Input.GetKey((KeyCode.Mouse0)))
+        {
+            useTargetRotation = true;
+            oldLockMode = Cursor.lockState;
+            Cursor.lockState = CursorLockMode.Locked;
+            if (resetRotationRoutine != null)
+            {
+                StopCoroutine(resetRotationRoutine);
+                resetRotationRoutine = null;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            float xRot = Input.GetAxis(axisYName);
+            float yRot = Input.GetAxis(axisXName);
+
+            cameraRot *= Quaternion.Euler(-xRot, 0F, 0F);
+            cameraRot = ClampRotationAroundXAxis(cameraRot);
+            focusRot *= Quaternion.Euler(0F, yRot, 0F);
+            focus.rotation = focusRot;
+            objectToFollow.transform.rotation = Quaternion.Euler(0, focus.rotation.eulerAngles.y, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1) && !Input.GetKey((KeyCode.Mouse0)))
+        {
+            useTargetRotation = true;
+            Cursor.lockState = oldLockMode;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (focus == null) return;
+        Gizmos.DrawLine(focus.position, focus.position + offset);
+        Gizmos.DrawWireSphere(focus.position, 0.5F);
+    }
+
+    IEnumerator EnableCameraFollowRotation()
+    {
+        yield return new WaitForSeconds(resetTime);
+        useTargetRotation = true;
+        resetRotationRoutine = null;
+    }
+}
